8d0b2a5ad59d3adebee14b57bfc339a08602f71e
jnthnexpdev <jnthnexpdev@gmail.com>
2025-12-11 19:41:11 -0600

    cv service

--- src/app/shared/services/cv-service.ts
+++ src/app/shared/services/cv-service.ts
@@ -8,7 +8,7 @@ export class CVService {
     public downloadCV() : void{
         const link = document.createElement('a');
         link.href = 'cv/CV_JonathanEspinosa_DesarrolladorWeb.pdf';
-        link.download = 'Jonathan_Espinosa_CV_Desarrollador_Web.pdf';
+        link.download = 'CV_JonathanEspinosa_DesarrolladorWeb.pdf';
         link.click();
     }
 
